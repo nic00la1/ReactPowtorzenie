d3e27b7438586b16c4edc11f1cdf2646ca6d24d4
Nicola Kaleta <99048749+nic00la1@users.noreply.github.com>
2026-01-19 09:35:18 +0100

    Add variables and update description in Component

--- components/Components.jsx
+++ components/Components.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 export const Component = () => {
+    let var1 = "Variable";
+    const var2 = "Constant variable";
+    const param = var1 + var2
+
     return (
         <React.Fragment>
             <h4>I am just a simple component</h4>
-            <h5>And this is my description</h5>
+            <h5>{var1} + {var2} = {param}</h5>
         </React.Fragment>
     )
 }
