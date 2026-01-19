f440af133d327ac89c70d5129bc53635468aa0e3
Nicola Kaleta <99048749+nic00la1@users.noreply.github.com>
2026-01-19 09:07:17 +0100

    Add simple React component with description

---
+++ components/Components.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Component = () => {
+    return (
+        <React.Fragment>
+            <h4>I am just a simple component</h4>
+            <h5>And this is my description</h5>
+        </React.Fragment>
+    )
+}
